9e839d982870c9114720203197f8e051813cc50a
[Renato Sortino] <[rensortino@gmail.com]>
2021-05-21 17:53:44 +0200

    Test with flattened output

--- ytid.py
+++ ytid.py
@@ -4,15 +4,16 @@ import pytorch_lightning as pl
 from log_utils import log_prediction
 import torch
 from torch import nn
+import torch.nn.functional as F
 import torchvision.transforms as T
 import math
-from data_processing import get_targets, append_tokens
+from data_processing import get_targets, append_tokens, process_labels
 import wandb
 
 TOKENS = {
-        "SOS": 11,
-        "EOS": 12,
-        "PAD": 13
+        "SOS": 0.0,
+        "EOS": 1.0,
+        "PAD": 0.5
     }
 
 class ImageGenerator(nn.Module):
@@ -28,19 +29,19 @@ class ImageGenerator(nn.Module):
             layers = [nn.ConvTranspose2d(in_feat, out_feat, kernel, stride, pad, bias=False)]
             return layers
 
-        #self.model = nn.Sequential(
-        #    *block(emb_size, ngf * 8, 4, 1, 0),
-        #    *block(ngf * 8, ngf * 4, 4, 2, 1),
-        #    *block(ngf * 4, ngf * 2, 4, 2, 1),
-        #    *block(ngf * 2, ngf, 4, 2, 1),
-        #    *block(ngf, ngf // 2, 4, 2, 1),
-        #    *block(ngf // 2, channels, 4, 2, 1, last=True)
-        #)
-
         self.model = nn.Sequential(
-            *block(emb_size, channels, 32, 1, 0, last=True),
-            #*block(ngf, channels, 4, 2, 1, last=True)
+            *block(emb_size, ngf * 8, 4, 1, 0),
+            *block(ngf * 8, ngf * 4, 4, 2, 1),
+            *block(ngf * 4, ngf * 2, 4, 2, 1),
+            *block(ngf * 2, ngf, 4, 2, 1),
+            *block(ngf, ngf // 2, 4, 2, 1),
+            *block(ngf // 2, channels, 4, 2, 1, last=True)
         )
+
+        #self.model = nn.Sequential(
+        #    *block(emb_size, channels, 32, 1, 0, last=True),
+        #    *block(ngf, channels, 4, 2, 1, last=True)
+        #)
 
     def forward(self, x):
         x = x.view(x.size(0) * x.size(1), self.emb_size, 1, 1)
@@ -102,7 +103,7 @@ class YTID(pl.LightningModule):
         )
 
         self.img_gen = img_gen
-        self.disc = disc
+        self.discriminator = disc
 
         # self.init_weights()
 
@@ -129,20 +130,27 @@ class YTID(pl.LightningModule):
         """
 
         # Create input embeddings
-        in_seq = self.embedding(src)* math.sqrt(self.args.model.emb_size)
-        # in_seq = in_seq.unsqueeze(1).view(self.args.model.seq_bs, self.args.model.seq_len, -1)
-        in_seq = in_seq.permute(1,0,2)
-        #in_seq = self.pos_enc(in_seq)
+        in_seq = self.embedding(src.int())* math.sqrt(self.args.model.emb_size)
+        in_seq = self.pos_enc(in_seq)
 
         # Forward transformer
-        # tgt[:-1] (shifted right because the transformer has to predict based on previous output)
         in_seq = in_seq.to(self.args.device)
         targets = targets.to(self.args.device)
-        tgt_mask = self.transformer.generate_square_subsequent_mask(targets.shape[0] - 1).to(self.args.device)
-        trf_out = self.transformer(in_seq, targets[:-1], tgt_mask=tgt_mask)
+        tgt_mask = self.transformer.generate_square_subsequent_mask(targets.shape[0]).to(self.args.device)
+        trf_out = self.transformer(in_seq, targets, tgt_mask=tgt_mask)
+        image_vector = trf_out[:,:,:-4]
+        bbox = trf_out[:,:,-4:]
 
-        out_imgs = self.img_gen(trf_out)
-        return out_imgs
+        # TODO Restore img_gen
+        #out_imgs = self.img_gen(trf_out)
+        out_imgs = image_vector.reshape(
+            image_vector.shape[0] * image_vector.shape[1],
+            self.args.data_loader.n_channels, 
+            self.args.data_loader.img_w, 
+            self.args.data_loader.img_h
+        )
+
+        return out_imgs, image_vector, bbox
 
     def make_src_mask(self, src):
         src_mask = src.transpose(0, 1) == self.src_pad_idx
@@ -150,35 +158,196 @@ class YTID(pl.LightningModule):
         # (N, src_len)
         return src_mask.to(self.device)
 
-    def training_step(self, batch, batch_idx):#, optimizer_idx):
+    def training_step(self, batch, batch_idx, optimizer_idx):
+
+        # Define Optimizers
+        g_opt, d_opt, t_opt, b_opt = self.optimizers()
 
         # Data loading
-        images, labels = batch
-        images = images.unsqueeze(1) # add the transformer sequence dimension
-        images = images.view(self.args.model.seq_len, self.args.model.seq_bs, self.args.data_loader.n_channels, self.args.data_loader.img_h, self.args.data_loader.img_w)
+        original_images, labels = batch
+        images = original_images.view(self.args.model.seq_len, self.args.model.seq_bs, self.args.data_loader.n_channels, self.args.data_loader.img_h, self.args.data_loader.img_w)
 
-        opt = self.optimizers()
-        opt.zero_grad()
+        targets, tgt_imgs = get_targets(self.feature_extractor, images)
+        #padded_tgt = get_padded_tgt(targets)
+
+        # tgt = [<SOS>, [embeddings], <EOS> (, [<PAD>] ) ]
+        # TODO Wrap in function
+        in_seq = labels.reshape(self.args.model.seq_bs, self.args.model.seq_len)
+        in_seq = process_labels(labels)
         
-        # Model forward
-        step_loss, predictions, target_imgs = self.transformer_step(labels, images)
+        # d_loss = self.discriminator_step(in_seq, targets, tgt_imgs)
 
-        self.manual_backward(step_loss)
-        opt.step()
+        # d_opt.zero_grad()
+        # self.manual_backward(d_loss)
+        # d_opt.step()
 
-        # Logging
-        #self.logger.experiment.log({'Train t_loss': step_loss, 'train_step': self.train_step})
-        self.logger.experiment.add_scalar('Loss/Train', step_loss, self.train_step)
-        self.log('Default Train t_loss', step_loss)
+        # self.log("d_loss", d_loss)
+
+        # wandb.log({"d_loss": d_loss})
+
+        #self.predictions, self.image_vectors, self.bbox = self(in_seq, targets[:-1])
+        
+        #g_loss = self.generator_step(tgt_imgs, targets)
+
+        # g_opt.zero_grad()
+        # self.manual_backward(g_loss, retain_graph=True)
+
+        t_loss, box_loss = self.transformer_step(in_seq, targets)
+
+        t_opt.zero_grad()
+        b_opt.zero_grad()
+        self.manual_backward(t_loss, retain_graph=True)
+        self.manual_backward(box_loss)
+
+        #g_opt.step()
+        t_opt.step()
+        b_opt.step()
+
+        self.log_dict({'t_loss': t_loss, "box_loss": box_loss}, prog_bar=True)
+
+
+        #TODO Wrap in function
         self.train_step += 1
         if self.global_step % self.args.trainer.log_every_n_steps == 0:
-            target_imgs = target_imgs.view(target_imgs.shape[0] * target_imgs.shape[1], self.args.data_loader.n_channels, target_imgs.shape[3], target_imgs.shape[4])
-            log_prediction(target_imgs, predictions, self.logger, title="Train Transformer Target and Ouptut")
+            target_imgs = tgt_imgs.view(tgt_imgs.shape[0] * tgt_imgs.shape[1], self.args.data_loader.n_channels, tgt_imgs.shape[3], tgt_imgs.shape[4])
+            log_prediction(target_imgs, self.predictions, self.logger, title="Train Transformer Target and Ouptut")
 
-        return step_loss
-        #return {'loss': step_loss, 'preds': predictions}
+    def discriminator_step(self, in_seq, targets, tgt_imgs):
+        ##########################
+        # Optimize Discriminator #
+        ##########################
+        # Measure discriminator's ability to classify real from generated samples
 
-    def transformer_step(self, labels, images):
+        # Transformer forward
+        # tgt[:-1] (shifted right because the transformer has to predict based on previous output)
+        # FIXME Use same prediction set or run multiple forward?
+        # self.predictions, self.image_vectors, self.bbox = self(in_seq, targets[:-1])
+        predictions, _, _ = self(in_seq, targets[:-1])
+
+        sl, bs, c, h, w = tgt_imgs.shape
+        tgt_imgs = tgt_imgs.reshape(sl * bs, c, h, w)
+
+        # how well can it label as real?
+        real = torch.ones(tgt_imgs.size(0), 1, device=self.args.device)
+
+        real_loss = self.adversarial_loss(self.discriminator(tgt_imgs), real)
+
+        # how well can it label as fake?
+        fake = torch.zeros(predictions.size(0), 1, device=self.args.device)
+
+        fake_loss = self.adversarial_loss(
+            self.discriminator(predictions.detach()), fake)
+
+        wandb.log({"d_real_loss": real_loss})
+        wandb.log({"d_fake_loss": fake_loss})
+
+        # discriminator loss is the sum of these
+        return real_loss + fake_loss
+
+    def generator_step(self, tgt_imgs, targets):
+        ######################
+        # Optimize Generator #
+        ######################
+
+        # adversarial loss is binary cross-entropy
+        sl, bs, c, h, w = tgt_imgs.shape
+        tgt_imgs = tgt_imgs.reshape(sl * bs, c, h, w)
+        real = torch.ones(tgt_imgs.size(0), 1, device=self.args.device)
+
+        g_loss = self.adversarial_loss(self.discriminator(self.predictions), real)
+
+        wandb.log({"g_loss": g_loss})
+
+        #self.log_dict({'t_loss': mse_loss, 'g_loss': t_adv_loss, 'd_loss': d_loss, 'd_real_loss': real_loss, 'd_fake_loss': fake_loss}, prog_bar=True)
+        return g_loss
+
+    def transformer_step(self, in_seq, targets):
+        ########################
+        # Optimize Transformer #
+        ########################
+
+        # TODO Check if processed input is the same as original input (no alteration has been done)
+        self.predictions, image_vectors, bbox = self(in_seq, targets[:-1])
+
+        tgt_vectors = targets[:,:,:-4]
+        tgt_bboxes = targets[:,:,-4:]
+
+        # tgt[1:] (shifted left to compare the real sequences, without the <SOS>)
+        t_loss = self.transformer_loss(self.criterion, image_vectors, tgt_vectors[1:])
+        # Exclude SOS and EOS
+        box_loss = self.transformer_loss(self.criterion, bbox[:-1], tgt_bboxes[1:-1])
+
+        # Compute loss
+        wandb.log({"t_loss": t_loss})
+        wandb.log({"box_loss": box_loss})
+
+        return  t_loss, box_loss
+
+    def adversarial_loss(self, y_hat, y):
+        return F.binary_cross_entropy(y_hat, y)
+
+    def transformer_loss(self, criterion,  outputs, targets):
+
+        targets = targets.reshape(targets.shape[0] * targets.shape[1], -1).to(self.args.device)
+        outputs = outputs.reshape(targets.shape).to(self.args.device)
+
+        loss = criterion(outputs, targets)        
+        return loss 
+
+    def custom_histogram_adder(self):
+        for name,params in self.named_parameters():
+            self.logger.experiment.add_histogram(name,params,self.current_epoch)
+
+    def training_epoch_end(self,outputs):
+        self.custom_histogram_adder()
+
+
+    #def validation_step(self, batch, batch_idx):
+    #    images, labels = batch
+    #    images = images.unsqueeze(1) # add the transformer sequence dimension
+    #    # Reshape images to seq_len, batch_size
+    #    images = images.view(self.args.model.seq_len, self.args.model.seq_bs, self.args.data_loader.n_channels, self.args.data_loader.img_h, self.args.data_loader.img_w)
+    #    step_loss, predictions, target_imgs = self.transformer_step(labels, images)
+    #    
+    #    # Logging
+    #    #self.logger.experiment.log({'Val t_loss': step_loss, 'val_step': self.val_step})
+    #    self.logger.experiment.add_scalar('Loss/Val', step_loss, self.val_step)
+    #    self.log("Default Transformer Loss", step_loss)
+#
+    #    self.val_step += 1
+    #    if self.global_step % self.args.trainer.log_every_n_steps == 0:
+    #        target_imgs = target_imgs.view(target_imgs.shape[0] * target_imgs.shape[1], self.args.data_loader.n_channels, target_imgs.shape[3], target_imgs.shape[4])
+    #        log_prediction(target_imgs, predictions, self.logger, title="Validation Transformer Target and Ouptut")
+    #    return step_loss
+#
+    def test_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self):
+        # FIXME Fix optimizers
+        g_optimizer = torch.optim.Adam(self.transformer.parameters(), lr=self.args.model.g_lr)
+        d_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=self.args.model.d_lr)
+        t_optimizer = torch.optim.Adam(self.transformer.parameters(), lr=self.args.model.t_lr)
+        b_optimizer = torch.optim.Adam(self.transformer.parameters(), lr=self.args.model.t_lr)
+
+        g_scheduler = torch.optim.lr_scheduler.StepLR(g_optimizer, 1.0, gamma=0.95)
+        d_scheduler = torch.optim.lr_scheduler.StepLR(d_optimizer, 1.0, gamma=0.95)
+        t_scheduler = torch.optim.lr_scheduler.StepLR(t_optimizer, 1.0, gamma=0.95)
+        b_scheduler = torch.optim.lr_scheduler.StepLR(b_optimizer, 1.0, gamma=0.95)
+
+
+        return [g_optimizer, d_optimizer, t_optimizer, b_optimizer], [g_scheduler, d_scheduler, t_scheduler, b_scheduler]
+
+
+
+
+
+
+
+
+
+
+    def transformer_step_old(self, labels, images):
         r'''
         Args:
         in_seq : (In_seq_len, N_batchs, Embedding)
@@ -198,61 +367,16 @@ class YTID(pl.LightningModule):
         in_list = append_tokens(in_seq.tolist(), TOKENS['EOS'], TOKENS['SOS'])
         in_seq = torch.tensor(in_list, device=self.args.device)
         out_imgs = self(in_seq, targets)
+        # ground truth result (ie: all fake)
+        # put on GPU because we created this tensor inside training_loop
+        valid = torch.ones(out_imgs.size(0), 1, device=self.args.device)
+
+        # adversarial loss is binary cross-entropy
+        g_loss = self.adversarial_loss(self.discriminator(out_imgs), valid)
+
         # Compute loss
         # tgt[1:] (shifted left to compare the real sequences, without the <SOS>)
-        t_loss = self.transformer_loss(self.criterion, out_imgs, tgt_imgs)
-        self.log("t_loss", t_loss)
-        wandb.log({"t_loss": t_loss})
-        return t_loss, out_imgs, tgt_imgs
-
-    def transformer_loss(self, criterion,  outputs, targets):
-
-        targets = targets.view(targets.shape[0] * targets.shape[1], -1)
-        outputs = outputs.view(targets.shape)
-
-        loss = criterion(outputs, targets)
-
-        # for i in range(outputs.shape[0]):
-        #     seq_loss = criterion(outputs[i], targets[i])
-        #     loss += seq_loss
-
-        
-        return loss #/ (outputs.shape[0] * outputs.shape[1])
-
-    def custom_histogram_adder(self):
-        for name,params in self.named_parameters():
-            self.logger.experiment.add_histogram(name,params,self.current_epoch)
-
-    def training_epoch_end(self,outputs):
-        #if(self.current_epoch==1):
-        #    sampleImg=torch.rand((4,16,512))
-        #    self.logger.experiment.add_graph(YTID(), sampleImg)
-
-        self.custom_histogram_adder()
-
-
-    def validation_step(self, batch, batch_idx):
-        images, labels = batch
-        images = images.unsqueeze(1) # add the transformer sequence dimension
-        # Reshape images to seq_len, batch_size
-        images = images.view(self.args.model.seq_len, self.args.model.seq_bs, self.args.data_loader.n_channels, self.args.data_loader.img_h, self.args.data_loader.img_w)
-        step_loss, predictions, target_imgs = self.transformer_step(labels, images)
-        
-        # Logging
-        #self.logger.experiment.log({'Val t_loss': step_loss, 'val_step': self.val_step})
-        self.logger.experiment.add_scalar('Loss/Val', step_loss, self.val_step)
-        self.log("Default Transformer Loss", step_loss)
-
-        self.val_step += 1
-        if self.global_step % self.args.trainer.log_every_n_steps == 0:
-            target_imgs = target_imgs.view(target_imgs.shape[0] * target_imgs.shape[1], self.args.data_loader.n_channels, target_imgs.shape[3], target_imgs.shape[4])
-            log_prediction(target_imgs, predictions, self.logger, title="Validation Transformer Target and Ouptut")
-        return step_loss
-
-    def test_step(self, batch, batch_idx):
-        pass
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.args.model.lr)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-        return [optimizer], [scheduler]
+        #t_loss = self.transformer_loss(self.criterion, out_imgs, tgt_imgs)
+        self.log("g_loss", g_loss)
+        wandb.log({"g_loss": g_loss})
+        return g_loss, out_imgs, tgt_imgs
